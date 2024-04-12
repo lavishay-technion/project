ac3db258b6d95b7b8de2ba1be9a893921bc325de
lavishay-technion <lavishay@campus.technion.ac.il>
2024-04-12 14:19:16 +0300

    Updating e_2

--- e_2.py
+++ e_2.py
@@ -1,13 +1,11 @@
 from e_1 import *
 final = {}
 a = readjson('departments.json')
-for i in a:
-    for a in a[i]:
+for i in a['departments']:
         sum_age = 0
-        department = a['name']
-        for s in a['employees']:
+        for s in i['employees']:
             sum_age += s['age']
-        print(f'In department {department} the avg age is {sum_age/len(a['employees'])}')
+        print(f'In department {i['name']} the avg age is {sum_age/len(i['employees'])}')
 
 
 
