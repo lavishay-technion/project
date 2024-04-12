6ff7d1cf1ad7b90032ed5f4ebd853791ee9f2673
avishay <avishayl.work@gmail.com>
2024-04-12 10:54:13 +0300

    First Commit

---
+++ e_1.py
@@ -0,0 +1,31 @@
+import json
+
+def readjson(file: str):
+    with open(file, 'r') as e:
+        data = json.load(e)
+    return data
+
+def incrementage(data):
+    for i in data:
+        i['age'] += 1
+    return data
+
+
+def writejson(file, data):
+    with open(file, "w") as r:
+        json.dump(data, r, indent = 2)
+
+def printold(data, filtered_age):
+    lst = []
+    for i in data:
+        if i['age']>filtered_age:
+            print(f'{i['name']} is old than 26,{i['name']} is {i['age']} old ')
+            lst.append(i)
+        
+    print(lst)
+
+
+a=readjson('names.json')
+# incrementage(a)
+# writejson('names.json', a)
+printold(a, 26)
