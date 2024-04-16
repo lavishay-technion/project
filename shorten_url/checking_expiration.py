4ca3fdb4e6d1b10f7c9f0bd0fff260a05b542cf6
lavishay-technion <lavishay@campus.technion.ac.il>
2024-04-16 20:05:56 +0300

    Short URL files update

---
+++ shorten_url/checking_expiration.py
@@ -0,0 +1,14 @@
+import json,datetime
+from datetime import datetime
+
+with open('D:\\Python\\PluralSight Course\\Technion Course\\Technion_Files\\project\\shorten_url\\short_urls.json', 'r') as e:
+    data = json.load(e)
+
+for i in data:
+    expiration = datetime.strptime(i['exp'], '%d/%m/%Y')
+    if expiration < datetime.now():
+        data.remove(i)
+with open('D:\\Python\\PluralSight Course\\Technion Course\\Technion_Files\\project\\shorten_url\\short_urls.json', 'w') as w:
+    json.dump(data, w, indent = 2)
+
+print('')
---
+++ shorten_url/shorten_URL.py
@@ -0,0 +1,38 @@
+import datetime
+from datetime import datetime
+import random
+import string
+import json
+
+
+
+def shorten_url(long_url, jsonfile: str,expiration ):
+    char = ''.join(random.choices(string.ascii_lowercase, k=5))
+    exists = False
+    with open(jsonfile, 'r') as e:
+        data = json.load(e)
+    for i in data:
+        if i['long'] == long_url:
+            exists = True
+            print(f'URL Already exists, the short url is:{i['short']}')
+    if exists == False:
+        short_url = f'short.com/{char}'
+        istrue = True
+        while istrue is True:
+            istrue = check_unique(data, short_url)
+        data.append({'long':long_url, 'short': short_url, 'exp':expiration})
+        with open(jsonfile, 'w') as r:
+            json.dump(data, r, indent = 2)
+    
+
+
+def check_unique(jsonfile, text):
+    jsonfile = jsonfile
+    stext = text
+    for i in jsonfile:
+        for key in i:
+            if key==text:
+                return True
+    return False
+
+shorten_url("https://github.com/lavishay-technion/project",'short_urls.json' , datetime(2024, 4, 17).strftime("%d/%m/%Y"))
