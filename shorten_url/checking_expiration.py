60835fff17f39c417145dd9e9fe6c73dcf33ac73
Avishay_Home <lavishay@campus.technion.ac.il>
2024-04-25 14:58:13 +0300

    updating

--- shorten_url/checking_expiration.py
+++ shorten_url/checking_expiration.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 with open('D:\\Python\\PluralSight Course\\Technion Course\\Technion_Files\\project\\shorten_url\\short_urls.json', 'r') as e:
     data = json.load(e)
+    print('')
 
 for i in data:
     expiration = datetime.strptime(i['exp'], '%d/%m/%Y')
